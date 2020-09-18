0cf33fb801cb03e3240ac049d5efba9264c9b892
cyka <1354250064mdzz@gmail.com>
2020-09-18 22:02:07 +0800

    guice first setup

---
+++ rpc-common/src/main/java/org/cyka/di/RpcCommonModule.java
@@ -0,0 +1,9 @@
+package org.cyka.di;
+
+import com.google.inject.AbstractModule;
+
+/** Class -> instance bind */
+public class RpcCommonModule extends AbstractModule {
+  @Override
+  protected void configure() {}
+}
