377c6590e10d374ab3a9c491d06b56d11f32f5a6
cyka <1354250064mdzz@gmail.com>
2020-09-30 22:10:02 +0800

    register EtcdServiceRegistry

--- rpc-common/src/main/java/org/cyka/di/RpcCommonModule.java
+++ rpc-common/src/main/java/org/cyka/di/RpcCommonModule.java
@@ -1,9 +1,17 @@
 package org.cyka.di;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import org.cyka.registry.ServiceRegistry;
+import org.cyka.registry.etcd.EtcdServiceRegistry;
 
 /** Class -> instance bind */
 public class RpcCommonModule extends AbstractModule {
   @Override
   protected void configure() {}
+
+  @Provides
+  ServiceRegistry serviceRegistry(String serviceRegistry) {
+    return new EtcdServiceRegistry();
+  }
 }
