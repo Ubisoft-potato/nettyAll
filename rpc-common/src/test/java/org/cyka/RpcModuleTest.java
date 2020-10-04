32304c213e3bc72ea35d5cf0219dc8a382707435
cyka <1354250064mdzz@gmail.com>
2020-10-04 21:38:11 +0800

    test etcd

--- rpc-common/src/test/java/org/cyka/RpcModuleTest.java
+++ rpc-common/src/test/java/org/cyka/RpcModuleTest.java
@@ -3,7 +3,6 @@ package org.cyka;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
-import com.google.inject.Provider;
 import lombok.extern.slf4j.Slf4j;
 import org.cyka.di.EtcdModule;
 import org.cyka.registry.ServiceRegistry;
@@ -15,7 +14,8 @@ public class RpcModuleTest {
   @Test
   public void etcdRegistry() {
     Injector injector = Guice.createInjector(new EtcdModule());
-    Provider<ServiceRegistry> registryProvider = injector.getProvider(Key.get(ServiceRegistry.class, EtcdModule.etcd.class));
-    registryProvider.get();
+    ServiceRegistry serviceRegistry =
+        injector.getInstance(Key.get(ServiceRegistry.class, EtcdModule.etcd.class));
+    serviceRegistry.register("test", 80);
   }
 }
